33736c6c27e8613d2dde6aea9bffad863a9e62c2
Rashmi <rashmi0505an@gmail.com>
2026-03-18 10:26:34 +0530

    Changed yml files to add lint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,18 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+
 export default [
+  js.configs.recommended,
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
+      globals: {
+        ...globals.browser,
+        ...globals.node
       }
     },
+    plugins: {
+      react,
+      "react-hooks": reactHooks
+    },
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "error"
+      "react/react-in-jsx-scope": "off"
     }
   }
 ];
