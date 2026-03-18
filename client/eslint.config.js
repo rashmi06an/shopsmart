bf55142d27c87f8594c40d12388734c44d4a9e6e
Rashmi <rashmi0505an@gmail.com>
2026-03-18 09:52:02 +0530

    Updated eslinet.config.js

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,21 +1,9 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
+export default [
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
-  pluginReact.configs.flat.recommended,
-]);
+    files: ["**/*.js", "**/*.jsx"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
+    }
+  }
+];
