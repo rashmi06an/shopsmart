31b70abbe9baab6831afa7528338422c7b001dea
Rashmi <rashmi0505an@gmail.com>
2026-03-18 09:54:03 +0530

    Updated eslint.config.js

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,6 +1,15 @@
 export default [
   {
     files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "error"
